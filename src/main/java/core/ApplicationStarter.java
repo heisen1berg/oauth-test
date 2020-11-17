602fd28f1b5be376ea3351ef6db266caef01cd29
heisen1berg <Lvtwnieoeet135>
2020-11-17 12:29:15 +0300

    demo

--- src/main/java/core/ApplicationStarter.java
+++ src/main/java/core/ApplicationStarter.java
@@ -1,4 +1,12 @@
 package core;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApplicationStarter {
+    public static void main(String[] args){
+        SpringApplication.run(ApplicationStarter.class);
+    }
 }
